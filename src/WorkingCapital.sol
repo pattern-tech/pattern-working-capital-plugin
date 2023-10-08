4cb36055f4c7e8de6e87bbcb68679fa62e837caa
alikhabazian <khabaziana@gmail.com>
2023-10-08 20:57:45 +0330

    add hasRemainingBudget functionality

--- src/WorkingCapital.sol
+++ src/WorkingCapital.sol
@@ -4,6 +4,8 @@
 pragma solidity 0.8.17;
 
 import {PluginCloneable, IDAO} from "@aragon/osx/core/plugin/PluginCloneable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import {BokkyPooBahsDateTimeLibrary} from "./BokkyPooBahsDateTimeLibrary.sol";
 
 contract WorkingCapital is PluginCloneable {
     /// @notice The ID of the permission required to call the `execute` function.
@@ -11,6 +13,14 @@ contract WorkingCapital is PluginCloneable {
         keccak256("ADMIN_EXECUTE_PERMISSION");
 
     address public admin;
+
+    uint256 public monthly_limit;
+
+    uint private this_month;
+
+    uint private this_year;
+
+    uint256 private this_month_remained;
 
     /// @notice Initializes the contract.
     /// @param _dao The associated DAO.
@@ -20,11 +30,41 @@ contract WorkingCapital is PluginCloneable {
         admin = _admin;
     }
 
+
+    /// @notice Checking that can user withdraw this amount
+    /// @param _actions actions that would be checked
+    function hasRemainingBudget(IDAO.Action[] calldata _actions){
+        uint _this_month = BokkyPooBahsDateTimeLibrary.getMonth(block.timestamp);
+        uint _this_year = BokkyPooBahsDateTimeLibrary.getYear(block.timestamp);
+        uint j=0;
+        for (; j < _actions.length; j+=1) {  //for loop example
+            // if we are on the month that we were
+            if(_this_month==this_month && _this_year==this_year){
+                require(this_month_remained>=_actions[j].value, string.concat("In ",Strings.toString(j)," action you want to spend more than your limit monthly") );
+                this_month_remained-=_actions[j].value;
+            }
+            else{
+                this_year = _this_year;
+                this_month = _this_month;
+                this_month_remained=monthly_limit;
+                require(this_month_remained>=_actions[j].value, string.concat("In ",Strings.toString(j)," action you want to spend more than your limit monthly") );
+                this_month_remained-=_actions[j].value;
+
+            }
+
+        }
+
+
+    }
+
+
+
     /// @notice Executes actions in the associated DAO.
     /// @param _actions The actions to be executed by the DAO.
     function execute(
         IDAO.Action[] calldata _actions
     ) external auth(ADMIN_EXECUTE_PERMISSION_ID) {
+        hasRemainingBudget(_actions);
         dao().execute({_callId: 0x0, _actions: _actions, _allowFailureMap: 0});
     }
 }
