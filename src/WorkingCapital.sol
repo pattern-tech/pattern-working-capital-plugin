5ebc5650518a1aab5c6d9aaaebdd43f82cf0892d
mehrdad <mehrdad@mehrdads-MacBook-Pro.local>
2023-10-09 03:21:39 +0330

    added hats integration, tested the input works but the whole DAO crashes

--- src/WorkingCapital.sol
+++ src/WorkingCapital.sol
@@ -4,27 +4,33 @@
 pragma solidity 0.8.17;
 
 import {PluginCloneable, IDAO} from "@aragon/osx/core/plugin/PluginCloneable.sol";
+import {IHats} from "./../hatsprotocol/src/Interfaces/IHats.sol";
 
 contract WorkingCapital is PluginCloneable {
-    /// @notice The ID of the permission required to call the `execute` function.
-    bytes32 public constant ADMIN_EXECUTE_PERMISSION_ID =
-        keccak256("ADMIN_EXECUTE_PERMISSION");
 
-    address public admin;
+    IHats public hatsProtocolInstance;
+    uint256 public hatId; 
+    uint256 public spendingLimitETH;
 
     /// @notice Initializes the contract.
     /// @param _dao The associated DAO.
-    /// @param _admin The address of the admin.
-    function initialize(IDAO _dao, address _admin) external initializer {
+    /// @param _hatId The id of the hat.
+    function initialize(IDAO _dao, uint256 _hatId, uint256 _spendingLimitETH) external initializer {
         __PluginCloneable_init(_dao);
-        admin = _admin;
+        hatId = _hatId;
+        // TODO get this from environment per network (this is goerli)
+        hatsProtocolInstance = IHats(0x3bc1A0Ad72417f2d411118085256fC53CBdDd137);
+        spendingLimitETH = _spendingLimitETH;
     }
 
     /// @notice Executes actions in the associated DAO.
     /// @param _actions The actions to be executed by the DAO.
     function execute(
         IDAO.Action[] calldata _actions
-    ) external auth(ADMIN_EXECUTE_PERMISSION_ID) {
+    ) external {
+        if(!hatsProtocolInstance.isWearerOfHat(msg.sender, hatId)){
+            revert("Sender is not wearer of the hat");
+        }
         dao().execute({_callId: 0x0, _actions: _actions, _allowFailureMap: 0});
     }
 }
