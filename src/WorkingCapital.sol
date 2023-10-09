792f9d776916d3a133f06d3e70dd0e185f966ee0
mehrdad <mehrdad0mms@gmail.com>
2023-10-09 11:13:28 +0330

    Revert "merge conflict fixed"
    This reverts commit 1468fc697117f3600352ce92040668fa87bd036e, reversing
    changes made to 690b30e6848db8a88a88246538bee2e86a03e8f1.

--- src/WorkingCapital.sol
+++ src/WorkingCapital.sol
@@ -4,6 +4,8 @@
 pragma solidity 0.8.17;
 
 import {PluginCloneable, IDAO} from "@aragon/osx/core/plugin/PluginCloneable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import {BokkyPooBahsDateTimeLibrary} from "./BokkyPooBahsDateTimeLibrary.sol";
 import {IHats} from "./../hatsprotocol/src/Interfaces/IHats.sol";
 
 contract WorkingCapital is PluginCloneable {
@@ -11,6 +13,10 @@ contract WorkingCapital is PluginCloneable {
     IHats public hatsProtocolInstance;
     uint256 public hatId; 
     uint256 public spendingLimitETH;
+
+    uint private currentMonth;
+    uint private currentYear;
+    uint256 private remainingBudget;
 
     /// @notice Initializes the contract.
     /// @param _dao The associated DAO.
@@ -23,14 +29,42 @@ contract WorkingCapital is PluginCloneable {
         spendingLimitETH = _spendingLimitETH;
     }
 
+    /// @notice Checking that can user withdraw this amount
+    /// @param _actions actions that would be checked
+    function hasRemainingBudget(IDAO.Action[] calldata _actions) internal {
+        uint _currentMonth = BokkyPooBahsDateTimeLibrary.getMonth(block.timestamp);
+        uint _currentYear = BokkyPooBahsDateTimeLibrary.getYear(block.timestamp);
+        uint j=0;
+        for (; j < _actions.length; j+=1) {  //for loop example
+            // if we are on the month that we were
+            if(_currentMonth==currentMonth && _currentYear==currentYear){
+                require(
+                    remainingBudget>=_actions[j].value,
+                    string.concat("In ",Strings.toString(j)," action you want to spend more than your limit monthly") 
+                );
+                remainingBudget-=_actions[j].value;
+            }
+            else{
+                currentYear = _currentYear;
+                currentMonth = _currentMonth;
+                remainingBudget=spendingLimitETH;
+                require(
+                    remainingBudget>=_actions[j].value, 
+                    string.concat("In ",Strings.toString(j)," action you want to spend more than your limit monthly") 
+                );
+                remainingBudget-=_actions[j].value;
+            }
+
+        }
+    }
+
     /// @notice Executes actions in the associated DAO.
     /// @param _actions The actions to be executed by the DAO.
     function execute(
         IDAO.Action[] calldata _actions
     ) external {
-        if(!hatsProtocolInstance.isWearerOfHat(msg.sender, hatId)){
-            revert("Sender is not wearer of the hat");
-        }
+        require(hatsProtocolInstance.isWearerOfHat(msg.sender, hatId), "Sender is not wearer of the hat");
+        hasRemainingBudget(_actions);
         dao().execute({_callId: 0x0, _actions: _actions, _allowFailureMap: 0});
     }
 }
