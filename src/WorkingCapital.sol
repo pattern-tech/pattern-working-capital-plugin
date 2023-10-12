258f3cdbd845e372a105ced0f7d181b4543292e7
Ali Khabazian <52274611+alikhabazian@users.noreply.github.com>
2023-10-12 23:54:47 +0330

    Update WorkingCapital.sol
    fix bugs about fixed size memory struct

--- src/WorkingCapital.sol
+++ src/WorkingCapital.sol
@@ -32,13 +32,13 @@ contract WorkingCapital is PluginCloneable {
     /// @notice Initializes the contract.
     /// @param _dao The associated DAO.
     /// @param _hatId The id of the hat.
-    function initialize(IDAO _dao, uint256 _hatId,address[] _token ,uint256[] _spendingLimit) external initializer {
+    function initialize(IDAO _dao, uint256 _hatId,address[] memory _token ,uint256[] memory _spendingLimit) external initializer {
         __PluginCloneable_init(_dao);
         hatId = _hatId;
         // TODO get this from environment per network (this is goerli)
         hatsProtocolInstance = IHats(0x3bc1A0Ad72417f2d411118085256fC53CBdDd137);
         require(_token.length==_spendingLimit.length,"Length of token address and spendingLimit array is not equal");
-        spendingLimit = _spendingLimit;
+//        spendingLimit = _spendingLimit;
         for (uint j=0; j < _token.length; j+=1) {
             spendingLimit[_token[j]]=_spendingLimit[j];
         }
@@ -48,7 +48,7 @@ contract WorkingCapital is PluginCloneable {
 
     /// @notice Check that the given token has allowance
     /// @param _token check that given token has allowance
-    function isTokenAvailable(address _token) internal pure returns (bool) {
+    function isTokenAvailable(address _token) internal view returns (bool) {
         for (uint256 i = 0; i < availableTokens.length; i++) {
             if (availableTokens[i] == _token) {
                 return true;
@@ -61,25 +61,32 @@ contract WorkingCapital is PluginCloneable {
     /// @notice Checking that can user withdraw this amount
     /// @param _actions actions that would be checked
     /// @return generatedDAOActions IDAO.Action generated for use in execute
-    function hasRemainingBudget(MyAction[] calldata _actions) internal returns(IDAO.Action[] generatedDAOActions){
+    function hasRemainingBudget(MyAction[] calldata _actions) internal returns(IDAO.Action[] memory generatedDAOActions){
         uint _currentMonth = BokkyPooBahsDateTimeLibrary.getMonth(block.timestamp);
         uint _currentYear = BokkyPooBahsDateTimeLibrary.getYear(block.timestamp);
+        generatedDAOActions = new IDAO.Action[](_actions.length);
         for (uint j=0; j < _actions.length; j+=1) {
             address _to;
             uint256 _value;
-            bytes _data;
-            if(_actions[j].ERC20.length == 0){
+            bytes memory _data;
+            address _token;
+            if(_actions[j].ERC20 == address(0)){
                 _to=_actions[j].to;
                 _value=_actions[j].value;
-                _data= bytes(0);
+                _data= new bytes(0);
+                _token=address(0);
+                require(isTokenAvailable(address(0)),"It is not available token in this plugin");
+
             }
             else{
-                _to=__actions[j].ERC20;
+                _to=_actions[j].ERC20;
                 _value=0;
                 bytes memory data = abi.encodeWithSignature("transfer(address,uint256)", _actions[j].to, _actions[j].value);
                 _data= data;
+                _token=_actions[j].ERC20;
+                require(isTokenAvailable(_actions[j].ERC20),"It is not available token in this plugin");
+
             }
-            require(isTokenAvailable(_token),"It is not available token in this plugin");
             // if we are on the month that we were
             if(_currentMonth==currentMonth && _currentYear==currentYear){
                 require(
@@ -102,25 +109,24 @@ contract WorkingCapital is PluginCloneable {
                 remainingBudget[_token]-=_actions[j].value;
             }
 
-            generatedDAOActions.push(IDAO.Action(_to, _value, _data));
+            generatedDAOActions[j]=IDAO.Action(_to, _value, _data);
 
         }
     }
 
-    //to value token
 
     /// @notice Executes actions in the associated DAO.
-    /// @param _actions The actions to be executed by the DAO.
+    /// @param _myActions The actions to be executed by the DAO.
     function execute(
         MyAction[] calldata _myActions
     ) external {
         require(hatsProtocolInstance.isWearerOfHat(msg.sender, hatId), "Sender is not wearer of the hat");
-        IDAO.Action [] idaoAction = hasRemainingBudget(_myActions);
+        IDAO.Action [] memory idaoAction = hasRemainingBudget(_myActions);
         dao().execute({_callId: 0x0, _actions: idaoAction, _allowFailureMap: 0});
     }
 
-    /// @param _spendingLimitETH The ETH spending limit
-    function updateSpendingLimit(uint256 _spendingLimitETH) external auth(UPDATE_SPENDING_LIMIT_PERMISSION_ID){
-        spendingLimitETH = _spendingLimitETH;
+    /// @param _spendingLimit spending limit
+    function updateSpendingLimit(address _token,uint256 _spendingLimit) external auth(UPDATE_SPENDING_LIMIT_PERMISSION_ID){
+        spendingLimit[_token] = _spendingLimit;
     }
 }
